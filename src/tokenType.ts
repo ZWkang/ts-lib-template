af61cf392a092dae50a1b6c4a3ab225211b4c198
zwkang <kang95630@gmail.com>
2023-03-09 11:42:28 +0800

    feat: init project

---
+++ src/tokenType.ts
@@ -0,0 +1,13 @@
+export const NumberLiteral = 'NumberLiteral';
+
+export const StringLiteral = 'StringLiteral';
+
+export const BooleanLiteral = 'BooleanLiteral';
+
+export const NullLiteral = 'NullLiteral';
+
+export const WhiteSpace = 'WhiteSpace';
+
+export const Punctuation = 'Punctuation';
+
+export const Identifier = 'Identifier';
